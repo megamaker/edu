1a0dc3eef821103d5fc8e3f624feecbec3a4a808
Jongtae Ham <boobykong@gmail.com>
2015-10-28 18:27:22 +0000

    led pwm
    modified:   sensor/potentiometer.py

--- sensor/potentiometer.py
+++ sensor/potentiometer.py
@@ -6,21 +6,26 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-a_pin = 18
-b_pin = 23
+powerPin = 18
+signalPin = 23
+ledPin = 24
+
+GPIO.setup(ledPin, GPIO.OUT)
+pwm = GPIO.PWM(ledPin, 500)
+pwm.start(0)
 
 def discharge():
-	GPIO.setup(a_pin, GPIO.IN)
-	GPIO.setup(b_pin, GPIO.OUT)
-	GPIO.output(b_pin, False)
+	GPIO.setup(powerPin, GPIO.IN)
+	GPIO.setup(signalPin, GPIO.OUT)
+	GPIO.output(signalPin, False)
 	time.sleep(0.05)
 
 def charge_time():
-	GPIO.setup(b_pin, GPIO.IN)
-	GPIO.setup(a_pin, GPIO.OUT)
+	GPIO.setup(signalPin, GPIO.IN)
+	GPIO.setup(powerPin, GPIO.OUT)
 	count = 0
-	GPIO.output(a_pin, True)
-	while not GPIO.input(b_pin):
+	GPIO.output(powerPin, True)
+	while not GPIO.input(signalPin):
 		count = count + 1
 	return count
 
@@ -29,5 +34,12 @@ def analog_read():
 	return charge_time()
 
 while True:
-	print analog_read()
-	time.sleep(1)
+	analogValue = analog_read()
+	print analogValue,
+	if analogValue < 30:
+		analogValue = 30
+	if analogValue > 300:
+		analogValue = 300
+	print analogValue / 3
+	pwm.ChangeDutyCycle(analogValue / 3)
+	time.sleep(0.05)
