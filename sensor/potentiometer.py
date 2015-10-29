48bf16717a9f51ca54d1d8f54c10f650fde13f99
Jongtae Ham <boobykong@gmail.com>
2015-10-29 15:14:27 +0900

    analog read data normalize
    pin number change
    modified:   sensor/potentiometer.py

--- sensor/potentiometer.py
+++ sensor/potentiometer.py
@@ -6,8 +6,8 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-powerPin = 18
-signalPin = 23
+powerPin = 23
+signalPin = 18
 ledPin = 24
 
 GPIO.setup(ledPin, GPIO.OUT)
@@ -17,14 +17,14 @@ pwm.start(0)
 def discharge():
 	GPIO.setup(powerPin, GPIO.IN)
 	GPIO.setup(signalPin, GPIO.OUT)
-	GPIO.output(signalPin, False)
+	GPIO.output(signalPin, GPIO.LOW)
 	time.sleep(0.05)
 
 def charge_time():
 	GPIO.setup(signalPin, GPIO.IN)
 	GPIO.setup(powerPin, GPIO.OUT)
+	GPIO.output(powerPin, GPIO.HIGH)
 	count = 0
-	GPIO.output(powerPin, True)
 	while not GPIO.input(signalPin):
 		count = count + 1
 	return count
@@ -33,13 +33,24 @@ def analog_read():
 	discharge()
 	return charge_time()
 
+def normalize(analogValue):
+	analogMin = 30
+	analogMax = 300
+	shiftMin = min - min
+	shiftMax = max - min
+	normalRange = float(shiftMax - shiftMin)
+
+	if analogValue < analogMin:
+		analogValue = analogMin
+	if analogValue > analogMax:
+		analogValue = analogMax
+
+	normalizeValue = (analogValue - analogMin) / normalRange
+	return int(normailizeValue * 100)
+
 while True:
 	analogValue = analog_read()
-	print analogValue,
-	if analogValue < 30:
-		analogValue = 30
-	if analogValue > 300:
-		analogValue = 300
-	print analogValue / 3
-	pwm.ChangeDutyCycle(analogValue / 3)
+	normalizeValue = normalize(analogValue)
+	print analogValue, normalizeValue
+	pwm.ChangeDutyCycle(normalizeValue)
 	time.sleep(0.05)
