23d2d8aa305b27fc0e753338f7a926de29fb5e27
Jongtae Ham <boobykong@gmail.com>
2015-11-08 11:44:14 +0000

    led subdir switch
    new file:   led/switch/switch.py
    renamed:    led/switch.png -> led/switch/switch_bb.png
    deleted:    led/switch.py

---
+++ led/switch/switch.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#encoding=utf-8
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+swPin = 18
+ledPin = 23
+
+GPIO.setup(swPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(ledPin, GPIO.OUT)
+
+while True:
+	input_state = GPIO.input(swPin)
+	if input_state == True:
+		GPIO.output(ledPin, GPIO.LOW)
+	else:
+		GPIO.output(ledPin, GPIO.HIGH)
