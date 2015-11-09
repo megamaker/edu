a2baea831985dae484ddb79a4b7424cf5454f832
Jongtae Ham <boobykong@gmail.com>
2015-11-09 05:15:27 +0000

    modified:   result/traffic/traffic.py

--- result/traffic/traffic.py
+++ result/traffic/traffic.py
@@ -4,47 +4,50 @@
 import RPi.GPIO as GPIO
 import time
 
-SW         = 12
-LED_RED    = 22
-LED_YELLOW = 24
-LED_GREEN  = 26
+def gpio_low():
+	GPIO.output(redPin,    GPIO.LOW)
+	GPIO.output(yellowPin, GPIO.LOW)
+	GPIO.output(greenPin,  GPIO.LOW)
 
-GPIO.setmode(GPIO.BOARD)
+swPin = 18
+redPin = 23
+yellowPin = 24
+greenPin = 25
 
-GPIO.setup(SW, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(LED_RED,    GPIO.OUT)
-GPIO.setup(LED_YELLOW, GPIO.OUT)
-GPIO.setup(LED_GREEN,  GPIO.OUT)
+GPIO.setup(swPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(redPin, GPIO.OUT)
+GPIO.setup(yellowPin, GPIO.OUT)
+GPIO.setup(greenPin, GPIO.OUT)
 
-GPIO.output(LED_RED,    GPIO.LOW)
-GPIO.output(LED_YELLOW, GPIO.LOW)
-GPIO.output(LED_GREEN,  GPIO.LOW)
+gpio_low()
 
-led_status = 0
-release_status = 0
-while True:
-	sw_status = GPIO.input(SW)
+try:
+	led_status = 0
+	release_status = 0
+	while True:
+		sw_status = GPIO.input(swPin)
 
-	if sw_status == False:
-		release_status = 1
-		print 'led_status', led_status
-		if led_status == 0:
-			GPIO.output(LED_RED,    GPIO.HIGH)
-			GPIO.output(LED_YELLOW, GPIO.LOW)
-			GPIO.output(LED_GREEN,  GPIO.LOW)
-		elif led_status == 1:
-			GPIO.output(LED_RED,    GPIO.LOW)
-			GPIO.output(LED_YELLOW, GPIO.HIGH)
-			GPIO.output(LED_GREEN,  GPIO.LOW)
-		elif led_status == 2:
-			GPIO.output(LED_RED,    GPIO.LOW)
-			GPIO.output(LED_YELLOW, GPIO.LOW)
-			GPIO.output(LED_GREEN,  GPIO.HIGH)
+		if sw_status == False:
+			release_status = 1
+			print 'led_status', led_status
+			if led_status == 0:
+				gpio_low()
+				GPIO.output(redPin, GPIO.HIGH)
+			elif led_status == 1:
+				gpio_low()
+				GPIO.output(yellowPin, GPIO.HIGH)
+			elif led_status == 2:
+				gpio_low()
+				GPIO.output(greenPin, GPIO.HIGH)
 
-	elif sw_status == True:
-		if release_status:
-			release_status = 0
-			led_status += 1
-			if led_status > 2:
-				led_status = 0
+		elif sw_status == True:
+			if release_status:
+				release_status = 0
+				led_status += 1
+				if led_status > 2:
+					led_status = 0
+except KeyboardInterrupt:
+	gpio_low()
+	GPIO.cleanup()
