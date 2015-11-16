1293e128a307b79a7931123af900ba8593c71220
Jongtae Ham <boobykong@gmail.com>
2015-11-16 14:31:52 +0000

    modified:   motor/servo_motor.py
    new file:   motor/servo_motor_bb.png
    new file:   result/temperature/temp_hum_bb.png

--- motor/servo_motor.py
+++ motor/servo_motor.py
@@ -4,26 +4,22 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-
 motor = 18
 
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(motor, GPIO.OUT)
 pwm = GPIO.PWM(motor, 100)
-pwm.start(0)
-
-#GPIO.output(motor, GPIO.HIGH)
+pwm.start(2.5)
 
 def update(angle):
 	duty = float(angle) / 10.0 + 2.5
 	print 'angle %d, duty %f' % (angle, duty)
 	pwm.ChangeDutyCycle(duty)
 
-update(90)
-while True:
-	angle = float(raw_input('angle : '))
-	if angle == -1:
-		break
-	update(angle)
-
-GPIO.cleanup()
+try:
+	while True:
+		for angle in [20, 45, 90, 150, 200, 150, 90, 40]:
+			update(angle)
+			time.sleep(1)
+except KeyboardInterrupt:
+	GPIO.cleanup()
